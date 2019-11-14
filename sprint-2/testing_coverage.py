f0a9d5c2a884f3f8f78520a4ce19c405af94016b
mccdqw <54416592+mccdqw@users.noreply.github.com>
2019-11-14 14:26:20 -0600

    Update testing_coverage.py

--- sprint-2/testing_coverage.py
+++ sprint-2/testing_coverage.py
@@ -14,7 +14,7 @@ FROM augur_data.repo_test_coverage JOIN augur_data.repo on repo_test_coverage.re
 GROUP BY augur_data.repo_test_coverage.repo_id
 """
 @annotate(tag='testing-coverage')
-def testing_coverage(self, repo_group_id, period='day', begin_date=None, end_date=None):
+def testing_coverage(self, repo_group_id, repo_id = 'None', period='day', begin_date=None, end_date=None):
 	"""
 	<the metric analzyes how much a repository is tested>
 	:parameter repo_group_id: The repository’s group id
@@ -22,24 +22,15 @@ def testing_coverage(self, repo_group_id, period='day', begin_date=None, end_dat
 	"""
 
 	if not begin_date:
-		begin_date = 1970-1-1 00:00:00'
+		begin_date = '1970-1-1 00:00:00'
 
 	if not end_date:
-		end_date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M
+		end_date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M)
 
 	testing_coverage_SQL = ' '
 							    
 	if not repo_id:
 		testing_coverage_SQL = s.sql.text("""
-		<repo_group form of the SQL query>
-	""")
-	results = pd.read_sql(testing-coverage-SQL>, self.database, params = {‘repo_group_id’: repo_group_id})
-	# output the testing coverage as percentages, one for subroutines tested and one for statements tested
-	return results
-
-	else:
-		testing_coverage_SQL> = s.sql.text("""
-			<repo form of the SQL query>
 		SELECT
 			augur_data.repo_test_coverage.repo_id,
 			augur_data.repo_test_coverage.file_subroutines_tested,
@@ -48,12 +39,29 @@ def testing_coverage(self, repo_group_id, period='day', begin_date=None, end_dat
 			augur_data.repo_test_coverage.file_statement_count
 		FROM augur_data.repo_test_coverage JOIN augur_data.repo on repo_test_coverage.repo_id = repo.repo_id
 		GROUP BY augur_data.repo_test_coverage.repo_id
+	
+		
 	""")
-		results = pd.read_sql(<testing-coverage-SQL>, self.database, params={‘repo_id’: repo_id})
-		# same as above for outputting percentages
+							    
+	results = pd.read_sql(testing-coverage-SQL>, self.database, params = {‘repo_group_id’: repo_group_id})
+	# output the testing coverage as percentages, one for subroutines tested and one for statements tested
 	return results
 
-@server.addRepoGroupMetric(metrics.testing-coverage, ‘testing-coverage’)
+	else:
+		testing_coverage_SQL> = s.sql.text("""
+		SELECT
+			augur_data.repo_test_coverage.file_subroutines_tested,
+			augur_data.repo_test_coverage.file_subroutine_count,
+			augur_data.repo_test_coverage.file_statements_tested,
+			augur_data.repo_test_coverage.file_statement_count
+		FROM augur_data.repo_test_coverage JOIN augur_data.repo on repo_test_coverage.repo_id = repo.repo_id
+		GROUP BY augur_data.repo_test_coverage.repo_id
+	""")
+	results = pd.read_sql(<testing-coverage-SQL>, self.database, params={‘repo_id’: repo_id})
+	# same as above for outputting percentages
+	return results
+
+server.addRepoGroupMetric(metrics.testing-coverage, ‘testing-coverage’)
 
 API Documentation
 """
