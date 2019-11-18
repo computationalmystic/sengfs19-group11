3f61752ee443af9f37176aced376ca7897bfa5fd
mccdqw <54416592+mccdqw@users.noreply.github.com>
2019-11-17 19:15:13 -0600

    Update test_coverage.py

--- sprint-2/test_coverage.py
+++ sprint-2/test_coverage.py
@@ -61,22 +61,6 @@ def testing_coverage(self, repo_group_id, repo_id = 'None', period='day', begin_
 	# same as above for outputting percentages
 	return results
 
-API Documentation
-"""
-@api {get} /repo/repo_id/testing-coverage Testing Coverage (Repo)
-@apiName testing-coverage-repo
-@apiGroup Risk
-@apiDescription Determine how much code has been tested for a repo 
-	 <a href="https://github.com/chaoss/wg-risk/blob/master/metrics/Test_Coverage.md">CHAOSS Metric Definition</a>
-@apiParam {string} repo_group_id Repository Group ID
-@apiParam {string} [begin_date="1970-1-1 0:0:0"] Beginning date specification. E.g. values: `2018`, `2018-05`, `2019-05-01`
-@apiParam {string} [end_date="current date"] Ending date specification. E.g. values: `2018`, `2018-05`, `2019-05-01`
-@apiSuccessExample {json} Success-Response:
-
-
-
-
-"""
-server.addRepoGroupMetric(metrics.testing_coverage, ‘testing-coverage’)							    
+							    
 
 
