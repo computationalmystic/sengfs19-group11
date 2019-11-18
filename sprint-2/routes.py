2bd679afa79d1ffbf69f6152d70d9be3735b5fb7
mccdqw <54416592+mccdqw@users.noreply.github.com>
2019-11-17 18:39:25 -0600

    Create routes.py

---
+++ sprint-2/routes.py
@@ -0,0 +1,17 @@
+from flask import Response
+
+
+
+API Documentation
+"""
+@api {get} /repo/repo_id/testing-coverage Testing Coverage (Repo)
+@apiName testing-coverage-repo
+@apiGroup Risk
+@apiDescription Determine how much code has been tested for a repo 
+	 <a href="https://github.com/chaoss/wg-risk/blob/master/metrics/Test_Coverage.md">CHAOSS Metric Definition</a>
+@apiParam {string} repo_group_id Repository Group ID
+@apiParam {string} [begin_date="1970-1-1 0:0:0"] Beginning date specification. E.g. values: `2018`, `2018-05`, `2019-05-01`
+@apiParam {string} [end_date="current date"] Ending date specification. E.g. values: `2018`, `2018-05`, `2019-05-01`
+@apiSuccessExample {json} Success-Response:
+"""
+server.addRepoGroupMetric(metrics.testing_coverage, ‘testing-coverage’)
