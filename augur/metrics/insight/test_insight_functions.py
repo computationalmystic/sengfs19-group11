9745136cb7c6c8cc606043ee84853b9432c6ed61
mccdqw <54416592+mccdqw@users.noreply.github.com>
2019-12-03 14:23:37 -0600

    Update test_insight_functions.py

--- augur/metrics/insight/test_insight_functions.py
+++ augur/metrics/insight/test_insight_functions.py
@@ -7,3 +7,6 @@ def metrics():
     import augur
     augur_app = augur.Application()
     return augur_app.metrics
+
+def test_commit_test_coverage(metrics):
+    assert metrics.commit_test_coverage(20).iloc[0].file_statement_count > 0
