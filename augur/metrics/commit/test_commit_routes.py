283645c383d45e915fddb13fd6b7c2fa32e1704a
Samantha Sample <40308900+ssample812@users.noreply.github.com>
2019-12-06 15:28:44 -0600

    Update test_commit_routes.py

--- augur/metrics/commit/test_commit_routes.py
+++ augur/metrics/commit/test_commit_routes.py
@@ -42,7 +42,7 @@ def test_annual_commit_count_ranked_by_repo_in_repo_group_by_group(metrics):
     assert data[0]["net"] > 0
 
 def test_committer_data(metrics):
-    response = requests.get('http://localhost:5000/api/unstable/repo-groups/20/committer-data')
+    response = requests.get('http://localhost:5000/api/unstable/repo-groups/20/committer-data', timeout=120)
     data = response.json()
     assert response.status_code == 200
     assert len(data) >= 1
