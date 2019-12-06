eb161f8215f5734723adf50a0de75ff1534c2e67
Samantha Sample <40308900+ssample812@users.noreply.github.com>
2019-12-06 14:42:03 -0600

    Update test_commit_functions.py

--- augur/metrics/commit/test_commit_functions.py
+++ augur/metrics/commit/test_commit_functions.py
@@ -17,4 +17,5 @@ def test_annual_commit_count_ranked_by_new_repo_in_repo_group(metrics):
     assert metrics.annual_commit_count_ranked_by_new_repo_in_repo_group(20, 21000).iloc[0].net > 0
 
 def test_committer_data(metrics):
-    assert metrics.committer_data(20).iloc[0].gender == 'male'
+    assert metrics.committer_data(20).iloc[6].gender == 'male'
+    assert metrics.committer_data(20).iloc[6].cmt_author_name == 'Alberto Martín'
