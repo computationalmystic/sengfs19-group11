eb35da20ebe6a7baafd158b5cc8c9a374f81a4fb
Samantha Sample <40308900+ssample812@users.noreply.github.com>
2019-12-06 15:05:15 -0600

    Update test_insight_routes.py

--- augur/metrics/insight/test_insight_routes.py
+++ augur/metrics/insight/test_insight_routes.py
@@ -5,8 +5,8 @@ import pytest
 def metrics():
     pass
 
-def test_commit_test_coverage(metrics):
-    response = requests.get('http://localhost:5000/api/unstable/repo-groups/20/commit-test-coverage')
+def test_testing_coverage(metrics):
+    response = requests.get('http://localhost:5000/api/unstable/repo-groups/20/testing-coverage')
     data = response.json()
     assert response.status_code == 200
     assert len(data) >= 1
